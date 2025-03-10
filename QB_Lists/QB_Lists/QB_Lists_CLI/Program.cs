6c960fa161dc820a47e0e08ac549ff538de88634
niharika98k <nsreekur@pnw.edu>
2025-03-10 02:49:04 +0000

    saved

---
+++ QB_Lists/QB_Lists/QB_Lists_CLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using com.intuit.idn.samples;
+using QBFC16Lib;
+
+class Program
+{
+    static void Main()
+    {
+        ItemInventoryQuery itemQuery = new ItemInventoryQuery();
+
+        itemQuery.DoItemInventoryQuery();
+
+    }
+}
